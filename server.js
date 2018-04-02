4c87df87dc006e88996a04161888da461818236d
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2018-04-01 21:37:03 -0500

    controllers

--- server.js
+++ server.js
@@ -2,9 +2,41 @@
 var express = require("express");
 var mongojs = require("mongojs");
 var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
 var request = require("request");
 var cheerio = require("cheerio");
+var axios = require('axios');
+
+// Require all models
+// var db = require("./models");
 
 // Initialize Express
 var app = express();
+
+var PORT = process.env.PORT || 3000;
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+// mongoose.Promise = Promise;
+// mongoose.connect(MONGODB_URI, {
+//   useMongoClient: true
+// });
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/news_scraper");
+
+
+
+     
