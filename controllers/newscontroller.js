e9a26ab00ee1bce8254c39817b2eb1dd97ea9e70
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2018-04-06 23:46:00 -0500

    note update

--- controllers/newscontroller.js
+++ controllers/newscontroller.js
@@ -5,7 +5,34 @@ module.exports = {
         models.Article.find({}).then(function(dbArticle){
             res.json(dbArticle);
         })
-    }
+    },
 
     //use same to write find , update, and delete functions
+    findOne: function(req, res) {
+        db.Article.findOne({_id: req.params.id}).then(function(dbArticle){
+            res.json(dbArticle);
+        })
+    },
+
+    delete: function(req, res) {
+        db.Article.remove(
+            {
+        //  delete article controller 
+        _id: mongojs.ObjectID(req.params.id)
+            
+
+        }, 
+        function(error, removed) {
+            if (error) {
+                console.log(error);
+                res.send(error);
+            } else {
+                console.log(removed);
+                res.send(removed);
+            }
+        }
+    )
+
+
+}
 }
